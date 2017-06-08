5b509de462e87711bc76d8feb38d0c0ec4d857f3
buddythumbs <moleary1987@gmail.com>
2017-06-08 22:23:56 +0100

    Adding in my own logo etc

--- app/js/components/Hello.js
+++ app/js/components/Hello.js
@@ -6,13 +6,13 @@ class Hello extends React.Component {
     return (
       <div>
         <img src={this.props.image} width={this.props.width} />
-        <h1>Welcome to Neuron!</h1>
+        <h1>Welcome to C.O.I. System!</h1>
       </div>
     )
   }
 }
 
 ReactDOM.render(
-  <Hello image={"dist/img/logo.png"} width={200} />,
+  <Hello image={"dist/img/coi-logo.png"} width={200} />,
   document.getElementById("app")
 )
